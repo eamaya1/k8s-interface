07c2f3cad4d8a4dea4518ebc89096dafa5d3ca11
Ebert Amaya <amayaebert1@outlook.com>
2025-06-07 22:18:03 -0400

    Adding initial pod functionality

--- main.go
+++ main.go
@@ -1,14 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/joho/godotenv"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	return os.Getenv(key)
+}
+
+func setupKubeconfig(pathToConfig string) (*rest.Config, error) {
+
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", pathToConfig)
+	if err != nil {
+		fmt.Printf("Something went wrong setting up the kubeconfig")
+		os.Exit(1)
+	}
+
+	return kubeconfig, nil
+}
 
 func main() {
 	//choices := []string{"pods", "services", "deployments"}
+	kubeconfig, err := setupKubeconfig(goDotEnvVariable("KUBECONFIG"))
+	if err != nil {
+		fmt.Printf("Invalid path to config")
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		fmt.Printf("There was an error creating the clientset. Check kubeconfig")
+		os.Exit(1)
+	}
+
+	// pods, err := clientset.CoreV1().Pods("kube-system").List(context.Background(), v1.ListOptions{})
+	// if err != nil {
+	// 	fmt.Printf("There was some error getting the pods from the namespace provided")
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println("Here are the pods in the selected namespace")
+	// for _, pod := range pods.Items {
+	// 	fmt.Println(pod.Name)
+	// }
+
+	var v viewController = newViewController(clientset)
+	vc := &v
 	p := tea.NewProgram(
 		//newKubeInterface(choices, 0, -1),
-		newViewController(),
+		vc,
 	)
 
 	if err := p.Start(); err != nil {
--- pod_views.go
+++ pod_views.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
+
+func getPods(clientset *kubernetes.Clientset) *corev1.PodList {
+	pods, err := clientset.CoreV1().Pods("kube-system").List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		fmt.Printf("There was an error getting the pods: %s", err)
+		os.Exit(1)
+	}
+
+	// Type v1.PodList, access it with pods.Items[].Name
+	return pods
+}
 
 // Defining our view model for any new screens we need to show
 type podView struct {
-	view string
+	view      string
+	clientset *kubernetes.Clientset
 }
 
-func newPodView(view string) podView {
+func newPodView(clientset *kubernetes.Clientset) podView {
 	s := "You are now looking at the Pods screen"
 	return podView{
-		view: s,
+		view:      s,
+		clientset: clientset,
 	}
 }
 
@@ -20,10 +40,17 @@ func (v podView) Init() tea.Cmd {
 	return nil
 }
 
+// There is a problem with clicking 0: pods, pointers may have been lost. Investigate that with the debugger if possible
 func (v podView) View() string {
-	v.view = "Here are the number of pods in your cluster:"
-	v.view += "\nPress ctrl+c to exit or backspace to return to main menu"
-	return v.view
+	pods := getPods(v.clientset)
+	numPods := len(pods.Items)
+	s := fmt.Sprintf("Here are the number of pods in your cluster: %d\n Here are the pods in your cluster:", numPods)
+
+	for _, pod := range pods.Items {
+		s += pod.Name + "\n"
+	}
+
+	return s
 }
 
 func (v podView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -33,7 +60,10 @@ func (v podView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c":
 			return v, tea.Quit
 		case "backspace":
-			return v, tea.Println("Returning to main screen") //Add the logic for this
+			return v, func() tea.Msg {
+				choices := []string{"pods", "services", "deployments"}
+				return switchViewMessage{newView: &kubeInterface{choices, 0, -1, v.clientset}}
+			} //Add the logic for this
 		}
 	}
 
